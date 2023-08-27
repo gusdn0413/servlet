25888de251bcd02249a4bb837f5213f8b1e59c45
hyunpark <gusdn0413@naver.com>
2023-08-28 04:00:25 +0900

    GET : 데이터를 가져오거나 조회 POST : 데이터를 서버에 제출

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,16 +15,18 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    //    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
-        return  "new-form";
+        return "new-form";
     }
 
-    @RequestMapping("/save")
-    public String  save(
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
-            Model model){
+            Model model) {
 
         Member member = new Member(username, age);
         memberRepository.save(member);
@@ -34,7 +35,8 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
 
